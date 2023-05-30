bfad057081c96214741f510258dfdfda97af555c
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-05-30 16:54:38 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Recipe from "./Recipe";
 import { recipe } from "./utils/data/recipe";
 import { sections } from "./utils/data/section";
 
+
 const App = () => {
   return <Recipe recipe={recipe} genericSections={sections} />;
 };
